15b3116104a5e2eb4d1dc2afb4bfef0e94b863ea
Brennan <brennan1086@gmail.com>
2023-01-13 14:55:19 -0800

    Added react-spring, Index mobile view has been updated

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,21 +1,69 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import LandingPage from '../images/LandingPageImage.jpg'
+import Logo from '../images/Logo.png'
 import { Link } from "react-router-dom";
+import { useSpring , useTransition , animated } from '@react-spring/web'
 
 function Index() {
+
+  const LogoFade = useSpring({
+    from: {opacity : 0},
+    to: { opacity : 1},
+    delay: 400,
+    config:{
+      duration : 900,
+    }
+  })
+
+  const headingSpring = useSpring({
+    from: { x: -800, },
+    to: { x: 0 },
+    delay : 300,
+    config:{
+      duration : 900,
+    }
+  })
+
+  const contentSpring = useSpring({
+    from: { y: 500, },
+    to: { y: 0 },
+    delay : 1200,
+    config:{
+      duration : 600,
+    }
+  })
+
+  const buttonAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity : 1 },
+    delay : 1900,
+    config:{
+      duration : 1400,
+    }
+  })
+
   return (
     <div className='h-screen'>
-        <figure className='m-0 h-1/3 w-full'>
-            <img src={LandingPage} className='object-cover w-full h-full'></img>
-        </figure>
-        <div className='h-2/3 w-full bg-gray-700 px-2 pt-3'>
-            <h1 className='text-4xl font-bold text-yellow-500'>Hunters Basecamp</h1>
-            <p className='text-white text-lg px-4'>Welcome to Hunters Basecamp the ultimate destination for hunters across North America. Connect with fellow hunters, share your experiences, and discover new hunting spots with our comprehensive mapping tools. Network with other hunters through our forums and messaging features. Find the best hunting spots, get the latest weather forecast and hunting regulations, and even plan your next hunting trip.</p>
-            <div className='flex w-full justify-evenly mt-5'>
-                <Link to={'Signup'} className='bg-yellow-600 w-28 h-10 rounded-lg text-white flex justify-center items-center'>Signup</Link>
-                <Link to={'Login'}  className='bg-yellow-600 w-28 h-10 rounded-lg text-white flex justify-center items-center'>Login</Link>
-            </div>
-            
+        <div className='h-full w-full bg-black px-2 pt-3'>
+            <animated.figure 
+            style={LogoFade}
+            className='m-0 absolute top-12 right-3 h-24 w-24'>
+              <img className='w-full h-full' src={Logo}></img>
+            </animated.figure>
+            <animated.h1
+            style={headingSpring}
+            className='text-5xl font-bold text-yellow-500 mt-8'
+            >Hunters Basecamp</animated.h1>
+            <animated.p 
+            style={contentSpring}
+            className='text-white text-lg px-4 mt-12'>Welcome to Hunters Basecamp the ultimate destination for hunters across North America. Connect with fellow hunters, share your experiences, and discover new hunting spots with our comprehensive mapping tools. Network with other hunters through our forums and messaging features. Find the best hunting spots, get the latest weather forecast and hunting regulations, and even plan your next hunting trip.</animated.p>
+            <animated.div 
+            style={buttonAnimation}
+            className='flex w-full justify-evenly mt-12 bg-black'>
+                <Link to={'Signup'} className='bg-yellow-500 w-28 h-10 rounded-lg  flex justify-center items-center'>Signup</Link>
+                <Link to={'Login'}  className='bg-yellow-500 w-28 h-10 rounded-lg  flex justify-center items-center'>Login</Link>
+            </animated.div>
+            <div className='bg-black h-full relative z-20'></div>     
         </div>
     </div>
   )
--- src/pages/SignInUp.js
+++ src/pages/SignInUp.js
@@ -1,8 +1,35 @@
 import React from 'react'
 
 function SignInUp() {
+  let currentPath = window.location.pathname
+
+  function Login(){
+    return(
+      <div>
+
+      </div>
+    )
+
+  }
+  function Signup(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+
   return (
-    <div>SignInUp</div>
+    <div className='h-screen bg-black'>
+      <h1 className='text-yellow-500 text-4xl font-bold ml-3 pt-3'>Hunters Basecamp</h1>
+      {
+        currentPath == '/Signup' && <div>signup</div>
+      }
+      {
+        currentPath == '/Login' && <div>login</div>
+      }
+    </div>
+    
   )
 }
 
